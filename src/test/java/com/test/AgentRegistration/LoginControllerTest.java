60cd58501e8facaf4a453ea08f3f2583f04f0664
Saswati Dash <M1053951@mindtree.com>
2020-01-16 12:51:16 +0530

    testcases correction

--- src/test/java/com/test/AgentRegistration/LoginControllerTest.java
+++ src/test/java/com/test/AgentRegistration/LoginControllerTest.java
@@ -5,6 +5,7 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -14,6 +15,7 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class LoginControllerTest {
 
     @Autowired
@@ -21,7 +23,7 @@ public class LoginControllerTest {
 
     @Test
     public void getLoginFormTest() throws Exception {
-        RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/registration");
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/login");
 
         MvcResult response = mockMvc.perform(requestBuilder).andReturn();
         //assertEquals("hello",registrationController.getRegistrationForm());
--- src/test/java/com/test/AgentRegistration/RegistrationControllerTest.java
+++ src/test/java/com/test/AgentRegistration/RegistrationControllerTest.java
@@ -4,9 +4,11 @@ import com.test.AgentRegistration.Controller.RegistrationController;
 import com.test.AgentRegistration.Entity.Agent;
 import com.test.AgentRegistration.Repository.AgentRepository;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -14,6 +16,8 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
@@ -23,6 +27,7 @@ import static org.mockito.Mockito.when;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class RegistrationControllerTest {
 
     @Autowired
@@ -31,11 +36,15 @@ public class RegistrationControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @Autowired
-    Agent agent;
+
 
     @MockBean
     private AgentRepository repository;
+   /* @Before
+    public void setUp() {
+        WebApplicationContext webApplicationContext;
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }*/
 
     @Test
     public void getRegistrationFormTest() throws Exception {
@@ -48,8 +57,8 @@ public class RegistrationControllerTest {
 
     @Test
     public void registerAgentTest() throws Exception {
-
-        when(repository.save(agent)).thenReturn(any());
+        Agent agent = new Agent();
+        //when(repository.save(agent)).thenReturn(any());
         RequestBuilder requestBuilder = MockMvcRequestBuilders.post("/registration");
         MvcResult response = mockMvc.perform(requestBuilder).andReturn();
         //assertEquals(response.getResponse().getContentType(),);
